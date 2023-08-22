b7d91110e17dc809f4197338ba662004a099fce7
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-08-22 19:41:27 +0300

    ready task41-v2

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,15 +1,36 @@
-﻿string s = "0, 7, 8, -2, -2";
+﻿
+// посложнее решил придумать)
+string s = "0, 7, 8, a, G,  -2, -2,YES";
 
-Console.WriteLine($"{String.Join(", ", GetPositiveNum(s))}");
-
+Console.WriteLine(GetPositiveNum(s));
 int GetPositiveNum(string s){
     string[] GetNumS = s.Split(',', StringSplitOptions.RemoveEmptyEntries);
     int count = 0;
-    foreach(string i in GetNumS){
-        int n = Convert.ToInt32(i);
-        if(n > 0){
-            count++;
+    foreach(string n in GetNumS){
+        bool tryN = int.TryParse(n, out int num);
+        if(tryN){
+            if(num > 0){
+                count++;
+            }
         }
     }
     return count;
 }
+
+
+
+// string s = "0, 7, 8, -2, -2";
+
+// Console.WriteLine($"{String.Join(", ", GetPositiveNum(s))}");
+
+// int GetPositiveNum(string s){
+//     string[] GetNumS = s.Split(',', StringSplitOptions.RemoveEmptyEntries);
+//     int count = 0;
+//     foreach(string i in GetNumS){
+//         int n = Convert.ToInt32(i);
+//         if(n > 0){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
