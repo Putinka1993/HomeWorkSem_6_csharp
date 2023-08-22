9224069b5d89bf063f8b7f391818ede240d7ddd8
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-08-22 18:49:56 +0300

    ready task41

---
+++ task41/Program.cs
@@ -0,0 +1,15 @@
+﻿string s = "0, 7, 8, -2, -2";
+
+Console.WriteLine($"{String.Join(", ", GetPositiveNum(s))}");
+
+int GetPositiveNum(string s){
+    string[] GetNumS = s.Split(',', StringSplitOptions.RemoveEmptyEntries);
+    int count = 0;
+    foreach(string i in GetNumS){
+        int n = Convert.ToInt32(i);
+        if(n > 0){
+            count++;
+        }
+    }
+    return count;
+}
